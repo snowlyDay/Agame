d977879e49c2a802357d286d64a55a012562bd2a
snowlyday <18753377106@163.com>
2017-03-18 15:19:01 +0800

    -------------- Please enter the commit message for your changes. Everything below this paragraph is ignored, and an empty message aborts the commit. Just close the window to accept your message. diff --git a/Agame/main.lua b/Agame/main.lua index f24c248..6d9275d 100644 --- a/Agame/main.lua +++ b/Agame/main.lua @@ -1,18 +1,28 @@
    +index =10
    +dt =2
    +function love.load()
    +	phyWord =love.physics.newWorld(0,10,false)
    +	playerBody =love.physics.newBody(phyWord,100,30,static)
    +    playerX= playerBody:getX()
    +	playerY =playerBody:getY()
    +	whale = love.graphics.newImage("peach.png")
    +    love.window.setIcon("peach.png")
    +end
    
    -function love.load()
    -	   Agameversion=1
    -	   love.graphics.setCaption("Agame")
    -	   iconimg=love.graphics.newImage("icon.gif")
    -	   love.graphics.setIcon(iconimg)
    -	   love.graphics.setBackgroundColor(0,0,0)
    -	   backgroundcolor={}
    -	   backgroundcolor[1] ={92,20,30}
    -
    +function World:update( dt )
    +
    +end
    +function love.update()
    +	if love.keyboard.isDown("right") then
    +	 	 index= index+dt
     	end
    +end
    +
    +
    
    -	function love.update(  )
    -	   love.graphics.setBackgroundColor(backgroundcolor[1]);
    -
    -	end
    \ No newline at end of file
    +function love.draw()
    +	love.graphics.draw(whale, playerX, playerY)
    +	love.graphics.setBackgroundColor(255,33,2)
    +end
    \ No newline at end of file
    diff --git a/Agame/peach.png b/Agame/peach.png
    new file mode 100644
    index 0000000..3277434
    Binary files /dev/null and b/Agame/peach.png differ
    diff --git a/Readme.md b/Readme.md
    index e69de29..821d233 100644
    --- a/Readme.md
    +++ b/Readme.md
    @@ -0,0 +1,2 @@
    +# Agame
    +lua

--- Agame/main.lua
+++ Agame/main.lua
@@ -1,18 +1,28 @@
 
+index =10
+dt =2
+function love.load()
+	phyWord =love.physics.newWorld(0,10,false)
+	playerBody =love.physics.newBody(phyWord,100,30,static)
+    playerX= playerBody:getX()	
+	playerY =playerBody:getY()
+	whale = love.graphics.newImage("peach.png")
+    love.window.setIcon("peach.png")
+end
 
 
-function love.load() 
-	   Agameversion=1
-	   love.graphics.setCaption("Agame")
-	   iconimg=love.graphics.newImage("icon.gif")
-	   love.graphics.setIcon(iconimg)
-	   love.graphics.setBackgroundColor(0,0,0)
-	   backgroundcolor={}
-	   backgroundcolor[1] ={92,20,30}
-
+function World:update( dt )
+	
+end
+function love.update()
+	if love.keyboard.isDown("right") then
+	 	 index= index+dt	
 	end
+end
 
-	function love.update(  )
-	   love.graphics.setBackgroundColor(backgroundcolor[1]);
-	   
-	end
+
+
+function love.draw()
+	love.graphics.draw(whale, playerX, playerY)
+	love.graphics.setBackgroundColor(255,33,2)
+end
